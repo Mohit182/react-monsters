41c671c165d6b161ece09211984bf0b9b9b17af3
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-08 15:45:54 +0530

    Updates

---
+++ precache-manifest.4650161e495a1aa3a10f7891502ad71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2b87c0069b12cfb6f396a2a4406a51",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "58ebed62e691bf6e3d63",
+    "url": "/react-monsters/static/css/main.7a11502c.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/react-monsters/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "58ebed62e691bf6e3d63",
+    "url": "/react-monsters/static/js/main.895c44ea.chunk.js"
+  },
+  {
+    "revision": "1c8ad7999933b0847d83",
+    "url": "/react-monsters/static/js/runtime~main.511ef2b8.js"
+  }
+]);
